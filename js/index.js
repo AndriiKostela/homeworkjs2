1fafa3c08673cb45a209a136ca60d534a486a3fc
Andrew Kostela <kostela1992@gmail.com>
2017-06-29 12:52:06 +0000

    Initial6

--- js/index.js
+++ js/index.js
@@ -1,9 +1,48 @@
 var n = Number(prompt('enter your n'));
-var fib = [1, 1];
-document.write('n-ne fib  = ' + fib (n));
-function fib(n) {
-    for (var i = 1; i < n - 1; i++) {
-        fib.push(fib[i] + fib[i - 1]);
-    }
-    return fib(n - 1) + fib(n - 2)}
+var fib= [];
+fib[0]=1;
+fib[1]=1;
+function fibonacci(n){ for( var i=2; i<n; i++){
+    fib[i]= fib[i-1]+ fib[i-2];
+}
+    return fib[n-1];
+}
+
+document.write(fibonacci(n)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fib = [1, 1];
+// document.write('n-ne fib  = ' + fib (n));
+// function fib(n) {
+//     for (var i = 1; i < n - 1; i++) {
+//         fib.push(fib[i] + fib[i - 1]);
+//     }
+//     return fib(n - 1) + fib(n - 2)}
 
