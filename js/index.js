d2f4b4d219e7601c5beffe7b93e0ef3429ced10a
Andrew Kostela <kostela1992@gmail.com>
2017-06-29 09:21:04 +0000

    initial4

--- js/index.js
+++ js/index.js
@@ -1,21 +1,9 @@
-var n = Number(prompt('..'));
-document.write('n-ne fib = ' + fib(n) + '<br>');
-
+var n = Number(prompt('enter your n'));
+var fib = [1, 1];
+document.write('n-ne fib  = ' + fib (n));
 function fib(n) {
-    if (n <= 2) {
-        return 1;
-    } else {
-        return fib(n - 1) + fib(n - 2);
-    }
-}
-
-n = Number(prompt('..'));
-var array_fib = [1, 1];
-document.write('n-ne fib array = ' + fibArray(n));
-
-function fibArray(n) {
     for (var i = 1; i < n - 1; i++) {
-       array_fib.push(array_fib[i] + array_fib[i - 1]);
+        fib.push(fib[i] + fib[i - 1]);
     }
-    return array_fib[array_fib.length - 1];
-}
+    return fib(n - 1) + fib(n - 2)}
+
